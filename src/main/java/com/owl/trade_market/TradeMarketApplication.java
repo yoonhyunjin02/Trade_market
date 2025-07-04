ab985039facd8380c089e9f791ce748464c750c8
yoonhyunjin <hyeonjin-hyunjin@naver.com>
2025-07-04 15:24:52 +0900

    🎉 초기 커밋 - 로컬 코드 업로드

---
+++ src/main/java/com/owl/trade_market/TradeMarketApplication.java
@@ -0,0 +1,13 @@
+package com.owl.trade_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/owl/trade_market/TradeMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owl.trade_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
