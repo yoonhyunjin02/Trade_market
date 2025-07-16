33fd959be8440cf32cc7d4c45eb2c21d0337fc5a
ksy <awers1010@gmail.com>
2025-07-16 14:46:41 +0900

    feat: 거래 생성 페이지에서 장소 입력 시, 자동완성 기능을 통해 주소를 추천하고, 선택된 주소를 지도에 반영하여 렌더링함.

--- src/main/resources/static/js/pages/write.js
+++ src/main/resources/static/js/pages/write.js
@@ -1,6 +1,11 @@
-let autocomplete, map, geocoder, userSetAddress, currentAddress
+let autocomplete, map, geocoder, tradeSetAddress
 // Google Maps API는 전역 객체 google.maps에 로딩됨. google.maps 바로 사용하면 됨.
 function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 37.5665, lng: 126.9780 },
+        zoom: 13,
+      });
+
 
     geocoder = new google.maps.Geocoder();
     const input = document.getElementById("location");
@@ -12,6 +17,7 @@ function initMap() {
       autocomplete.addListener("place_changed", onPlaceSelected); // (이벤트, 선택된 주소) 자동완성에서 주소를 선택하면 이벤트 발생
 
 }
+// 해당 장소 상세정보 가져오기
 function onPlaceSelected() {
     const place = autocomplete.getPlace(); // 해당 장소 상세정보를 가져오는 기능
     if (!place.geometry) {
@@ -19,4 +25,14 @@ function onPlaceSelected() {
         return;
     }
 
+    tradeSetAddress = place.formatted_address;
+
+    map.setCenter(place.geometry.location);
+
+    if(window._selMarker) window._selMarker.setMap(null);
+    window._selMarker = new google.maps.Marker({
+        position: place.geometry.location,
+        map: map,
+        title: "당근할 장소",
+    })
 }
