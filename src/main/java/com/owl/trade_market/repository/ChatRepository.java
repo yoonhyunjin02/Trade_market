b25ddfa363666f0747ee5b3394bbca4cc689ed96
SooowanLee <lsy9406@gmail.com>
2025-07-16 09:46:00 +0900

    Feat: 마지막 메시지 1건 조회 기능 추가

--- src/main/java/com/owl/trade_market/repository/ChatRepository.java
+++ src/main/java/com/owl/trade_market/repository/ChatRepository.java
@@ -1,6 +1,7 @@
 package com.owl.trade_market.repository;
 
 import com.owl.trade_market.entity.Chat;
+import com.owl.trade_market.entity.ChatRoom;
 import com.owl.trade_market.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -9,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
@@ -24,4 +26,7 @@ public interface ChatRepository extends JpaRepository<Chat, Long> {
     // 읽지 않은 메시지가 있는 채팅방 ID 목록 조회
     @Query("SELECT DISTINCT c.chatRoom.id FROM Chat c WHERE (c.chatRoom.buyer = :user OR c.chatRoom.product.seller = :user) AND c.isRead = false AND c.userId != :userId")
     List<Long> findChatRoomIdsWithUnreadMessagesForUser(@Param("user") User user, @Param("userId") String userId);
+
+    // 특정 채팅방의 마지막 메시지 1건을 조회합니다.
+    Optional<Chat> findTopByChatRoomOrderByIdDesc(ChatRoom chatroom);
 }
