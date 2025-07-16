428dc6bce6cf1526eaaf3d6afd70981d6730b99c
SooowanLee <lsy9406@gmail.com>
2025-07-16 09:23:30 +0900

    Feat: ChatRepository에 읽지 않은 메시지 카운트 및 읽음 처리, 채팅방 ID 조회 기능 추가

---
+++ src/main/java/com/owl/trade_market/repository/ChatRepository.java
@@ -0,0 +1,27 @@
+package com.owl.trade_market.repository;
+
+import com.owl.trade_market.entity.Chat;
+import com.owl.trade_market.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface ChatRepository extends JpaRepository<Chat, Long> {
+
+    // 읽지 않은 메시지 수 카운트
+    long countByChatRoomIdAndUserIdNotAndIsReadIsFalse(Long chatRoomId, String currentUserId);
+
+    //상대방이 보낸 모든 메시지를 읽음으로 처리
+    @Modifying
+    @Transactional
+    @Query("UPDATE Chat c SET c.isRead = true WHERE c.chatRoom.id = :chatRoomId AND c.userId != :currentUserId AND c.isRead = false")
+    void markAsReadByRoomIdAndUserId(@Param("chatRoomId") Long chatRoomId, @Param("currentUserId") String currentUserId);
+
+    // 읽지 않은 메시지가 있는 채팅방 ID 목록 조회
+    @Query("SELECT DISTINCT c.chatRoom.id FROM Chat c WHERE (c.chatRoom.buyer = :user OR c.chatRoom.product.seller = :user) AND c.isRead = false AND c.userId != :userId")
+    List<Long> findChatRoomIdsWithUnreadMessagesForUser(@Param("user") User user, @Param("userId") String userId);
+}
